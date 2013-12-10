c1a76d781c726ed305b4dbb6b90f5dec3dfa1b71
adams3 <a.studenic@gmail.com>
2013-12-10 08:55:57 +0100

    database connection

--- php/database.php
+++ php/database.php
@@ -2,12 +2,20 @@
 
 require dirname(__FILE__) . '/../libs/dibi/dibi/dibi.php';
 
+//$options = array(
+//    'driver' => 'mysql',
+//    'host' => 'localhost',
+//    'username' => 'root',
+//    'password' => '',
+//    'database' => 'support_db',
+//);
+
 $options = array(
     'driver' => 'mysql',
     'host' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'support_db',
+    'username' => 'adminv2jhrsc',
+    'password' => '76f-iK4Jy6uF',
+    'database' => 'support',
 );
 
 // v případě chyby vyhodí DibiException
