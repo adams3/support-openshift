66ef42cb20911d593e12ce07b6ce72f26b9a89bc
adams3 <a.studenic@gmail.com>
2014-02-22 10:59:20 +0100

    db

--- php/database.php
+++ php/database.php
@@ -1,27 +1,35 @@
-<?php
-require dirname(__FILE__) . '/../libs/dibi/dibi/dibi.php';
+ <?php
+ require dirname(__FILE__) . '/../libs/dibi/dibi/dibi.php';
 
-$options = array(
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'support_db',
-);
+ //$options = array(
+ //    'driver' => 'mysql',
+ //    'host' => 'localhost',
+ //    'username' => 'root',
+ //    'password' => '',
+ //    'database' => 'support_db',
+ //);
 
-//$options = array(
-//    'driver' => 'mysql',
-//    'host' => 'localhost',
-//    'username' => 'adminv2jhrsc',
-//    'password' => '76f-iK4Jy6uF',
-//    'database' => 'support',
-//);
+ define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+ define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
+ define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+ define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+ define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 
-// v případě chyby vyhodí DibiException
-try {
-    dibi::connect($options);
-} catch (DibiException $e) {
-    die("error db connection");
-};
+ $options = array(
+     'driver' => 'mysql',
+     'host' => DB_HOST,
+     'username' => DB_USER,
+     'password' => DB_PASS,
+-    'database' => DB_NAME,
++    'database' => DB_NAME
+ );
 
-?>
+ // v případě chyby vyhodí DibiException
+ try {
+     dibi::connect($options);
+ } catch (DibiException $e) {
+ var_dump($e);
+     die("error db connection");
+ };
+
+ ?>
